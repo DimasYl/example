52094e6ce8d20d64e20e78bd2d38894277f5134a
DimasYl <dima.ilyushin.2016@mail.ru>
2021-03-18 17:07:31 +0300

    test2

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -13,7 +13,7 @@ const Post = (props: PostPropsType) => {
             <img src='https://movies4maniacs.liberty.me/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg'  alt={''}/>
             { props.message }
             <div>
-                <span>likes</span> { props.likesCount }
+                <span>like</span> { props.likesCount }
             </div>
         </div>
     )
